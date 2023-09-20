7243d29181a68cd1e0f679518386bcf07bc71b97
Daniel Bedoya <95carloshdn@gmail.com>
2023-09-20 08:30:38 -0500

    Exercise.Py
    software que calcule si un aprendiz tiene el estilo de aprendizaje Asimilador.

---
+++ PythonGit/Exercise.py
@@ -0,0 +1,10 @@
+"""
+ Fecha 20/09/2023 
+ Autor: Daniel Bedoya 
+ Objetivo: Se requiere un software que calcule si un aprendiz tiene el estilo de aprendizaje Asimilador. 
+ Para ello deben realizarse 7 preguntas de respuesta SI o NO.
+ Si el aprendiz obtuvo 4 o más respuestas en SI
+entonces el sistema deberá indicarle que es Asimilador, de lo contrario le dirá que su estilo de aprendizaje es otro.
+"""
+
+   
